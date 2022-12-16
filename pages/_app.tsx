d18ed89c6d0c409e9c13e3dd1aa3b34824f2c4a7
Kevin <kphyde@gmail.com>
2022-12-16 14:04:29 -0800

    configure basic pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,22 @@
-import { useState } from "react";
-import type { AppProps } from "next/app";
+import { useState } from 'react'
+import type { AppProps } from 'next/app'
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx } from "theme-ui";
-import { ThemeProvider } from "theme-ui";
-import theme from "../styles/theme";
-import "../styles/globals.css";
-import Nav from "../src/components/nav";
-import Head from "next/head";
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import { jsx } from 'theme-ui'
+import { ThemeProvider } from 'theme-ui'
+import theme from '../styles/theme'
+import '../styles/globals.css'
+import Nav from '../src/components/nav'
+import Head from 'next/head'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 
 export default function App({
   Component,
   pageProps,
 }: AppProps<{ initialSession: Session }>) {
   // Create a new supabase browser client on every first render.
-  const [supabase] = useState(() => createBrowserSupabaseClient());
+  const [supabase] = useState(() => createBrowserSupabaseClient())
 
   return (
     <SessionContextProvider
@@ -26,12 +26,20 @@ export default function App({
       <Head>
         <title>MUSH-A-BOOM!</title>
         <meta name="description" content="Mushroom cataloging application" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="theme-color" content="#000" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
       </Head>
       <ThemeProvider theme={theme}>
         <Nav />
         <Component {...pageProps} />
       </ThemeProvider>
     </SessionContextProvider>
-  );
+  )
 }
