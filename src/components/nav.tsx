719ea626287fab2cf91323a45424c12372b8f150
Ryan Cybul <ryan.cybul@gmail.com>
2022-12-20 18:54:38 -0500

    Added Mr Mushroom.  Adjusted Nav styles slightly

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,27 +1,30 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 const Nav = () => {
-  const router = useRouter()
-  const expanded = router.pathname === '/'
+  const router = useRouter();
+  const expanded = router.pathname === '/';
 
   return (
     <header>
       <nav className={expanded ? 'expanded' : ''}>
-        <Link href="/">
+        <Image src='/images/MushroomManCropped.png' width='110' height='110' />
+        <Link href='/'>
           <h1>MUSHROOM BOOM!</h1>
         </Link>
         <div>
-          <Link href="/addmushroom/">
-            <button className="add">🍄</button>
+          <Link href='/addmushroom/'>
+            <button className='add'>Add a mushroom</button>
           </Link>
-          <Link href="/mushrooms/">
-            <button className="mushrooms">Mushroom List</button>
+          <Link href='/mushrooms/'>
+            <button className='mushrooms'>Mushroom List</button>
           </Link>
+          <button>Sign In / Sign Up</button>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
