da6f0a602699d814d82220b736cd65d3e4e81b83
Kevin <kphyde@gmail.com>
2022-12-20 20:48:48 -0800

    fix TS on db.tsx

--- utils/db.tsx
+++ utils/db.tsx
@@ -1,60 +1,72 @@
-export async function handleCreate(supabase: object, mushroom: object) {
+import { SupabaseClient } from '@supabase/supabase-js'
+import { Mushroom } from '../pages/addmushroom'
+
+export async function handleCreate(
+  supabase: SupabaseClient,
+  mushroom: Mushroom,
+) {
   try {
-    const { error } = await supabase.from('mushroom').insert(mushroom).single();
-    if (error) throw error;
-    alert('Mushroom record created!');
+    const { error } = await supabase.from('mushroom').insert(mushroom).single()
+    if (error) throw error
+    alert('Mushroom record created!')
   } catch (error) {
-    alert('Error creating mushroom record');
-    console.log(error);
+    alert('Error creating mushroom record')
+    console.log(error)
   }
 }
 
-export async function handleUpdate(supabase: object, mushroom: object) {
+export async function handleUpdate(
+  supabase: SupabaseClient,
+  mushroom: Mushroom,
+) {
   try {
     const { error } = await supabase
       .from('mushroom')
       .update(mushroom)
-      .eq('id', id);
-    if (error) throw error;
-    alert('Mushroom record updated!');
+      .eq('id', mushroom.id)
+    if (error) throw error
+    alert('Mushroom record updated!')
   } catch (error) {
-    alert('Error updating mushroom record');
-    console.log(error);
+    alert('Error updating mushroom record')
+    console.log(error)
   }
 }
 
-export async function handleGetAll(supabase: object, setMushrooms: object) {
+export async function handleGetAll(
+  supabase: SupabaseClient,
+  setMushrooms: (data: unknown) => void,
+) {
   try {
-    const { data, error } = await supabase.from('mushroom').select('*');
-    if (error) throw error;
-    setMushrooms(data);
+    const { data, error } = await supabase.from('mushroom').select('*')
+    if (error) throw error
+    setMushrooms(data)
   } catch (error) {
-    alert('Error loading mushroom records.');
-    console.log(error);
+    alert('Error loading mushroom records.')
+    console.log(error)
   }
 }
 
-export async function handleGetOne(supabase: object, id: string) {
+export async function handleGetOne(supabase: SupabaseClient, id: string) {
   try {
     const { data, error } = await supabase
       .from('mushroom')
       .select('*')
-      .eq('id', id);
-    if (error) throw error;
-    return data;
+      .eq('id', id)
+    if (error) throw error
+    return data
   } catch (error) {
-    alert('Error loading mushroom record.');
-    console.log(error);
+    alert('Error loading mushroom record.')
+    console.log(error)
   }
 }
 
-export async function handleDeleteOne(supabase: object, id: string) {
+export async function handleDeleteOne(supabase: SupabaseClient, id: string) {
   try {
-    const { error } = await supabase.from('mushroom').delete('*').eq('id', id);
-    if (error) throw error;
-    alert('Mushroom record deleted.');
+    const { error } = await supabase.from('mushroom').delete('*').eq('id', id)
+    if (error) throw error
+    alert('Mushroom record deleted.')
   } catch (error) {
-    alert('Error deleting mushroom record.');
-    console.log(error);
+    alert('Error deleting mushroom record.')
+    console.log(error)
   }
 }
