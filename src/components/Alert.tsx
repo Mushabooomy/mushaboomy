7665f58edfae916ed845417fcdc7951412021ca8
Kevin <kphyde@gmail.com>
2022-12-21 18:26:14 -0800

    alert styles

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Alert({ message, type }: AlertProps) {
-  return <div className={type}>{message}</div>
+  return <div className={`alert ${type}`}>{message}</div>
 }
